1d05d37238de1791f6d25d718da952eb180a8e2d
iancu_robert <robert.iancu@zupria.com>
2023-08-06 17:09:52 +0300

    Add checkboxes with functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
-import { Component, ContentChildren, QueryList, AfterContentInit, AfterViewInit, ViewChildren } from '@angular/core';
+import {
+  Component,
+  ContentChildren,
+  QueryList,
+  AfterContentInit,
+  AfterViewInit,
+  ViewChildren,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 import { TableService } from './services';
 import { NestedTableBodyRowDirective } from './nested-table/nested-table-body-row.directive';
 
@@ -10,6 +19,12 @@ import { NestedTableBodyRowDirective } from './nested-table/nested-table-body-ro
 export class AppComponent implements AfterViewInit {
   @ViewChildren(NestedTableBodyRowDirective)
   rows?: QueryList<NestedTableBodyRowDirective>;
+
+  @ViewChild('headerRowCheckbox')
+  headerRowCheckbox?: ElementRef<HTMLInputElement>;
+
+  @ViewChildren('bodyRowCheckbox')
+  bodyRowCheckboxes?: QueryList<ElementRef<HTMLInputElement>>;
 
   data$ = this.tableService.getData();
 
@@ -25,6 +40,43 @@ export class AppComponent implements AfterViewInit {
     rootRow.toggle();
   }
 
+  onHeaderCheckboxValueChange() {
+    if (!this.headerRowCheckbox) return;
+
+    const checkbox = this.headerRowCheckbox.nativeElement;
+
+    this.bodyRowCheckboxes?.forEach((bodyCheckbox) => {
+      bodyCheckbox.nativeElement.checked = checkbox.checked;
+    });
+  }
+
+  onBodyRowCheckboxValueChange(bodyRow: NestedTableBodyRowDirective) {
+    const bodyRowCheckboxesValues = this.bodyRowCheckboxes?.map((item) => (item.nativeElement.checked ? 1 : 0)) ?? [];
+
+    const numOfCheckedRows = bodyRowCheckboxesValues.reduce((acc: number, value: number) => acc + value, 0);
+
+    const isAllChecked = numOfCheckedRows === this.rows?.length;
+    const isIndeterminate = numOfCheckedRows >= 1 && !isAllChecked;
+
+    if (this.headerRowCheckbox) {
+      this.headerRowCheckbox.nativeElement.checked = isAllChecked;
+      this.headerRowCheckbox.nativeElement.indeterminate = isIndeterminate;
+    }
+  }
+
+  private initRowsState() {
+    if (!this.rows) return;
+
+    for (let i = 0; i < this.rows.length; i++) {
+      const row = this.rows.get(i);
+
+      if (row?.parent) {
+        row.hide();
+      } else {
+        row?.collapse();
+      }
+    }
+  }
   private toggleRowsRecursively(rootRow: NestedTableBodyRowDirective) {
     if (!this.rows) return;
 
@@ -41,20 +93,6 @@ export class AppComponent implements AfterViewInit {
         if (row.isExpanded) {
           this.toggleRow(row);
         }
-      }
-    }
-  }
-
-  private initRowsState() {
-    if (!this.rows) return;
-
-    for (let i = 0; i < this.rows.length; i++) {
-      const row = this.rows.get(i);
-
-      if (row?.parent) {
-        row.hide();
-      } else {
-        row?.collapse();
       }
     }
   }
